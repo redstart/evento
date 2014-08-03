dca2de5ff15277ff3d791e482d048bec5aed2507
redstartme@gmail.com <redstartme@gmail.com>
2014-08-04 03:41:42 +0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Evento::Application.config.secret_key_base = '80d628808a2de16c73794ef761f389c7f9c1b8efac5d9a85f1ab9550de9da5825ac5a2eeaa9ad38b4bc37575c52019ee650aef01a4061862b01be3a1d522d6ad'
